7b1fca3d82d3ae705da35e8d45c7a9822ff37579
William Surjana <williamsurjana2013@gmail.com>
2020-11-18 12:37:55 +0700

    Create Method

---
+++ Quiz2/src/main/java/Controller/MainProgram.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.image.ImageObserver;
+import java.util.ArrayList;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author A442UF
+ */
+public class MainProgram {  
+
+    public static void main(String[] args){
+        new Screen();
+    }
+}
